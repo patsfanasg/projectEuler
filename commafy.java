117f6b4c870c89424d0057c34bc67e3d4851b37a
Aidan Gannon <aidansgannon@icloud.com>
2018-09-18 09:25:05 -0400

    Started Coding

--- commafy.java
+++ commafy.java
@@ -1,5 +1,16 @@
+
 public class commafy{
-	public static void main(String[]args){
-	
+	public static String commafy(int x){
+		String x = "";
+
+		if(x<1000){
+			return x;
+		}
+		else{
+			x = num/1000 + "," + num%1000;
+
+			
+		}
 	}
+
 }
