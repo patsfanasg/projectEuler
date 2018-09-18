9ef7ab8bd80f5b621880f31147e217444e148da5
Aidan Gannon <aidansgannon@icloud.com>
2018-09-18 09:09:40 -0400

    Removed For Loop
    For Loop wouldn't work

--- problem2.java
+++ problem2.java
@@ -4,12 +4,12 @@ public class problem2{
 	}
 	public static int sumFib(int max){
 		int sum =0;
-		while(sum< 4000000){
-		for(int i = 1; i < max; i++){
+		int i = 0;
+		while(sum< max){
 			if((i+(i+1))%2==0){
 			sum = sum + i + (i+1);
-		}
-	}
+			}
+			i++;
 }
 return sum;
 }
