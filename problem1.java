04137a6097b0c553a6cf2b9165ae85b1c5617fce
Aidan Gannon <aidansgannon@icloud.com>
2018-09-18 08:55:45 -0400

    Edit Test
    Edited test

--- problem1.java
+++ problem1.java
@@ -1,9 +1,9 @@
 public class problem1{
 	public static void main(String[]args){
-		int x = 25;
+		int x = 10;
 		System.out.println(getMultiples(x));
 	}
-	public int getMultiples(int max){
+	public static int getMultiples(int max){
 		int sum = 0;
 		for(int i = 0; i <= max; i++){
 			if(i%3==0 || i%5==0){
