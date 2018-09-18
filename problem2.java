f475fb3f043bef0c5a3842212f88acc38fbad9ff
Aidan Gannon <aidansgannon@icloud.com>
2018-09-18 09:11:02 -0400

    New Variable
    New fib variable required

--- problem2.java
+++ problem2.java
@@ -4,9 +4,11 @@ public class problem2{
 	}
 	public static int sumFib(int max){
 		int sum =0;
-		int i = 0;
-		while(sum< max){
-			if((i+(i+1))%2==0){
+		int i = 1;
+		int fib = 0;
+		while(fib< max){
+			fib = i + (i+1);
+			if(fib%2==0){
 			sum = sum + i + (i+1);
 			}
 			i++;
