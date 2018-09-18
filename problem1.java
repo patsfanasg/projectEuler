4ae7bbb8932ec476046be0eb25a9481a96e62b23
Aidan Gannon <aidansgannon@icloud.com>
2018-09-18 08:59:12 -0400

    Fixed UpperBound
    Was adding up to 33 not 23, needed to change <= to <

--- problem1.java
+++ problem1.java
@@ -5,9 +5,10 @@ public class problem1{
 	}
 	public static int getMultiples(int max){
 		int sum = 0;
-		for(int i = 0; i <= max; i++){
+		for(int i = 0; i < max; i++){
 			if(i%3==0 || i%5==0){
 				sum = sum + i;
+				System.out.println(i);
 			}
 		}
 		return sum;
