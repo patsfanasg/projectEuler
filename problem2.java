c24714c8187d7749852396e7dcd1f4baa0286763
Aidan Gannon <aidansgannon@icloud.com>
2018-09-18 09:17:57 -0400

    Futher edit
    Still fixing

--- problem2.java
+++ problem2.java
@@ -1,17 +1,25 @@
 public class problem2{
 	public static void main(String[]args){
-
+		int x = 4000000;
+		System.out.println(sumFib(x));
 	}
 	public static int sumFib(int max){
-		int sum =0;
+		int sum = 0;
 		int i = 1;
+		int j = 2;
 		int fib = 0;
 		while(fib< max){
-			fib = i + (i+1);
+			fib = i + j;
+			System.out.println(fib);
 			if(fib%2==0){
-			sum = sum + i + (i+1);
+
+			sum = sum + (i + (i+1));
 			}
-			i++;
+			i = j;
+			j = fib;
+
+
 }
 return sum;
+}
 }
